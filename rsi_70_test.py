cbb769a91dc7e976cc44e701c3d7070cd784eecb
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-04-16 10:16:47 +0300

    restructure files

--- rsi_70_test.py
+++ rsi_70_test.py
@@ -1,16 +1,17 @@
 # %%
+import client as cl
 import pandas as pd
 import pandas_ta as ta
 import matplotlib.pyplot as plt
 plt.style.use("fivethirtyeight")
 
 # %%
-get_historical_candles("BTCBUSD", "2h", "September 01, 2017", "September 01, 2020")
+data = cl.get_historical_candles("BTCUSDT", "2h", "September 01, 2017", "September 01, 2020")
 data.head()
 
 # %%
 plt.figure(figsize=(12.5, 4.5))
-plt.plot(data["Close"], label = "BTC - BUSD")
+plt.plot(data["Close"], data["Close time"], label = "BTC - BUSD")
 plt.title("")
 plt.xlabel(data.iloc[0]["Close time"] + " between " + data.iloc[-1]["Close time"])
 plt.ylabel("BTC close prices(USD)")
@@ -19,4 +20,6 @@ plt.show()
 
 # %%
 sma_30 = pd.Dataframe()
-sma_30["Close price"]
+sma_30["Close"] = ta.sma(data["Close"])
+sma_30
+# %%

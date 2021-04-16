910b5ff729129ae8e34b67216a207d528eab3fd0
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-04-16 09:48:51 +0300

    update data get and save functions

--- core/binance/client.py
+++ core/binance/client.py
@@ -3,6 +3,8 @@
 # %%
 import config
 import pandas as pd
+import os.path
+from os import path
 from binance.client import Client
 
 # %%
@@ -10,7 +12,7 @@ client = Client(config.api_key, config.api_secret)
 
 # %%
 def save_historical_candles(symbol, interval, start_time, end_time) :
-    file_name = symbol + "_" + interval + ".csv"
+    file_name = symbol + "_" + interval + "_" + start_time + "_" + end_time + ".csv"
     
     columns_name = ["Open time", "Open", "High", "Low", "Close", "Volume", "Close time", 
             "Quote asset volume", "Number of trades", "Taker buy base asset volume", 
@@ -25,4 +27,13 @@ def save_historical_candles(symbol, interval, start_time, end_time) :
 
     return(data)
 
+# %%
+def get_historical_candles(symbol, interval, start_time, end_time) : 
+    file_name = symbol + "_" + interval + "_" + start_time + "_" + end_time + ".csv"
+    if ( path.exists(file_name) ) : 
+        data = read_from_csv("BTCBUSD_2h.csv")
+        return data
+    else : 
+        data = save_historical_candles(symbol, interval, start_time, end_time) 
+        return data
 # %%

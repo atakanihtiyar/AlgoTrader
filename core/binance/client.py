ddbec647c130fdea5cbeceb3d9407f97e90925ce
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-04-16 07:54:22 +0300

    create client with using api key and api secret from config file

--- core/binance/client.py
+++ core/binance/client.py
@@ -1,34 +1,8 @@
 # To add a new cell, type '# %%'
 # To add a new markdown cell, type '# %% [markdown]'
 # %%
-import pandas as pd
+import config
 from binance.client import Client
-
-
-# %%
-def save_historical_candles(symbol, interval, start_time, end_time) :
-    file_name = symbol + "_" + interval + ".csv"
-    
-    columns_name = ["Open time", "Open", "High", "Low", "Close", "Volume", "Close time", 
-            "Quote asset volume", "Number of trades", "Taker buy base asset volume", 
-            "Taker buy quote asset volume", "Can be ignored"]
-    
-    data_json = client.get_historical_klines(symbol, interval, start_time, end_time)
-    
-    data = pd.DataFrame(data_json, columns = columns_name)
-    data['Close time'] = pd.to_datetime(data['Close time'], unit='ms')
-    data['Open time'] = pd.to_datetime(data['Open time'], unit='ms')
-    data.to_csv(file_name, encoding='utf-8')
-    
-    return data
 
 # %%
 client = Client(config.api_key, config.api_secret)
-
-
-# %%
-save_historical_candles("BTCBUSD", "2h", "January 01, 2017", "January 01, 2021")
-
-
-
-# %%

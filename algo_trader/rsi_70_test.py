d607535fb7649bcee2b6047007b9f97c06af0388
Berkay Atakan İhtiyar <26187453+atakanihtiyar@users.noreply.github.com>
2021-04-16 11:48:43 +0300

    calculate indicators

--- algo_trader/rsi_70_test.py
+++ algo_trader/rsi_70_test.py
@@ -19,8 +19,25 @@ plt.legend(loc = "upper left")
 plt.show()
 
 # %%
-sma_30 = pd.DataFrame()
-sma_30["Close"] = ta.sma(data["Close"])
-sma_30
+sma_9 = pd.DataFrame()
+sma_9["Close"] = ta.sma(data["Close"], 9)
+sma_9
 # %%
+sma_100 = pd.DataFrame()
+sma_100["Close"] = ta.sma(data["Close"], 100)
+sma_100
 # %%
+rsi_14 = pd.DataFrame()
+rsi_14["Close"] = ta.rsi(data["Close"], 14)
+rsi_14
+# %%
+plt.figure(figsize=(12.5, 4.5))
+plt.plot(data["Close"], label = "BTC - BUSD")
+plt.plot(sma_9["Close"], label = "SMA 9")
+plt.plot(sma_100["Close"], label = "SMA 100")
+plt.plot(rsi_14["Close"], label = "RSI 14")
+plt.title("")
+plt.xlabel(data.iloc[0]["Close time"] + " between " + data.iloc[-1]["Close time"])
+plt.ylabel("BTC close prices(USD)")
+plt.legend(loc = "upper left")
+plt.show()
